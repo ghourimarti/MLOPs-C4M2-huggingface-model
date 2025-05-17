fa24a42d768d4f7263e347b08c78bc2b3314bf1a
Zain Ul Abdin <ghourimarti@gmail.com>
2025-05-17 12:30:06 +0500

    Initial commit for Hugging Face MLOps project

---
+++ transfor.py
@@ -0,0 +1,11 @@
+import torch
+from transformers import pipeline
+
+chat = [
+    {"role": "system", "content": "You are a sassy, wise-cracking robot as imagined by Hollywood circa 1986."},
+    {"role": "user", "content": "Hey, can you tell me any fun things to do in New York?"}
+]
+
+pipeline = pipeline(task="text-generation", model="meta-llama/Meta-Llama-3-8B-Instruct", torch_dtype=torch.bfloat16, device_map="auto")
+response = pipeline(chat, max_new_tokens=512)
+print(response[0]["generated_text"][-1]["content"])
